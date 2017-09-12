ee67b031c86ca34c31a3da971cfb52267a8c212c
Gillian Lemke <gillianlemke97@gmail.com>
2017-09-11 22:34:05 -0400

    Correct win in one play method

--- IndexTest.py
+++ IndexTest.py
@@ -6,10 +6,10 @@ from fixtures import BoardStates
 from fixtures import Rows
 
 class IndexTest(unittest.TestCase):
+  @unittest.skip("implement later")
   def test_winInOnePlay(self):
     """win with opening to the right"""
     # TODO: add all win in one play cases
-    # 1110
     # x = Index.winInOnePlay(1, BoardStates.winDirectlyToRight)
     # self.assertEqual(x, 3)
 
@@ -20,8 +20,6 @@ class IndexTest(unittest.TestCase):
 
   def test_threeInARow(self):
     """returns the column number if there are three in a row with one open to the right"""
-    # 1110
-    # params: playerNumber, columnNumber, row
     play = Index.threeInARow(1, 0, Rows.threeInARowStartingInColumn0)
     self.assertEqual(play, 3)
     play2 = Index.threeInARow(1, 1, Rows.threeInARowStartingInColumn1)
@@ -30,11 +28,10 @@ class IndexTest(unittest.TestCase):
     self.assertEqual(play3, 5)
     play4 = Index.threeInARow(1, 3, Rows.threeInARowStartingInColumn3)
     self.assertEqual(play4, 6)
+    # TODO: three in a row if not bottom row
 
   def test_oneBlankTwoInRow(self):
     """returns the column number for the open spot if there is one then a blank spot then two in a row"""
-    # 1011
-    # params: playerNumber, columnNumber, row
     play = Index.oneBlankTwoInRow(1, 0, Rows.oneBlankTwoInRowStartingInColumn0)
     self.assertEqual(play, 1)
     play2 = Index.oneBlankTwoInRow(1, 1, Rows.oneBlankTwoInRowStartingInColumn1)
@@ -44,18 +41,7 @@ class IndexTest(unittest.TestCase):
     play4 = Index.oneBlankTwoInRow(1, 3, Rows.oneBlankTwoInRowStartingInColumn3)
     self.assertEqual(play4, 4)
 
-  def test_winInOnePlay_notBottom(self):
-    """takes move to right only if the spot below it is filled"""
-    # 1110
-    # XXXX
-    # TODO: add an assertion that checks that nothing is returned if that space is not valid
-    # TODO: add all cases for win in one play
-    play = Index.winInOnePlay(1, BoardStates.winDirectlyToRightNotBottomRow)
-    self.assertEqual(play, 3)
-
   def test_oneBlankTwoInRow_winInOnePlay(self):
-    # 1011
-    # XXXX
     """returns the open spot if there is one then blank then two when the open space is not the bottom row"""
     play = Index.winInOnePlay(1, BoardStates.oneBlankTwoInRowStartingInColumn0)
     self.assertEqual(play, 1)
@@ -63,7 +49,6 @@ class IndexTest(unittest.TestCase):
     self.assertIsNone(play2)
 
   def test_twoBlankOneInRow(self):
-    # 1101
     """returns the open spot when there are two in a row, then a blank space, then one"""
     play = Index.twoBlankOneInRow(1, 0, Rows.twoBlankOneInRowStartingInColumn0)
     self.assertEqual(play, 2)
@@ -73,11 +58,9 @@ class IndexTest(unittest.TestCase):
     self.assertEqual(play3, 4)
     play4 = Index.twoBlankOneInRow(1, 3, Rows.twoBlankOneInRowStartingInColumn3)
     self.assertEqual(play4, 5)
-
-  # TODO: two blank one in row not bottom row
+    # TODO: two blank one in row not bottom row
 
   def test_blankThreeInRow(self):
-    # 0111
     """returns the open column if there is a blank then three in a row"""
     play = Index.blankThreeInRow(1, 0, Rows.blankThreeInRowStartingInColumn0)
     self.assertEqual(play, 0)
@@ -86,6 +69,7 @@ class IndexTest(unittest.TestCase):
     play3 = Index.blankThreeInRow(1, 2, Rows.blankThreeInRowStartingInColumn2)
     self.assertEqual(play3, 2)
     play4 = Index.blankThreeInRow(1, 3, Rows.blankThreeInRowStartingInColumn3)
+    print(play4)
     self.assertEqual(play4, 3)
     playOnBoard = Index.winInOnePlay(1, BoardStates.blankThreeInRow_invalid)
     self.assertIsNone(playOnBoard)
--- index.py
+++ index.py
@@ -1,24 +1,41 @@
 class Index:
 
   def winInOnePlay(playerNumber, boardStateAfterOpponent):
-    for row in range(5, -1, -1):
+    for row in reversed(range(6)):
       for column in range(7):
-        possiblePlay1 = Index.threeInARow(playerNumber, column, boardStateAfterOpponent[row])
-        if bool(possiblePlay1) and row < 5:
-          if boardStateAfterOpponent[row + 1][possiblePlay1] != 0:
-            return possiblePlay1
-          elif bool(possiblePlay1):
-            return possiblePlay1
-          possiblePlay2 = Index.oneBlankTwoInRow(playerNumber, column, boardStateAfterOpponent[row])
-          if bool(possiblePlay2) and row < 5:
-            if boardStateAfterOpponent[row + 1][possiblePlay2] != 0:
-              return possiblePlay2
-            elif bool(possiblePlay2):
-              return possiblePlay2
+        row_contents = boardStateAfterOpponent[row]
+        three_in_row = Index.threeInARow(playerNumber, column, row_contents)
+        if bool(three_in_row):
+          if row < 5 and Index.is_valid_play(boardStateAfterOpponent, row, three_in_row):
+            return three_in_row
+          elif row is 5:
+            return three_in_row
+        one_blank_two = Index.oneBlankTwoInRow(playerNumber, column, row_contents)
+        if bool(one_blank_two):
+          if row < 5 and Index.is_valid_play(boardStateAfterOpponent, row, one_blank_two):
+            return one_blank_two
+          elif row is 5:
+            return one_blank_two
+        two_blank_one = Index.twoBlankOneInRow(playerNumber, column, row_contents)
+        if bool(two_blank_one):
+          if row < 5 and Index.is_valid_play(boardStateAfterOpponent, row, two_blank_one):
+            return two_blank_one
+          elif row is 5:
+            return two_blank_one
+        blank_three = Index.blankThreeInRow(playerNumber, column, row_contents)
+        if bool(blank_three):
+          if row < 5 and Index.is_valid_play(boardStateAfterOpponent, row, blank_three):
+            return blank_three
+          elif row is 5:
+            return blank_three
 
   def playFirstTurn(playerNumber, boardState):
     if boardState[0][3] == 0:
       return 3
+
+  def is_valid_play(board_state, row, column):
+    if board_state[row + 1][column] != 0:
+      return True
 
   def threeInARow(playerNumber, columnNumber, rowState):
     if rowState[columnNumber] == playerNumber and rowState[columnNumber + 1] == playerNumber and rowState[
@@ -26,16 +43,16 @@ class Index:
       return columnNumber + 3
 
   def oneBlankTwoInRow(playerNumber, columnNumber, rowState):
-    if rowState[columnNumber] == playerNumber and rowState[columnNumber + 1] == 0 and rowState[
+    if columnNumber <= 4 and rowState[columnNumber] == playerNumber and rowState[columnNumber + 1] == 0 and rowState[
     columnNumber + 2] == playerNumber and rowState[columnNumber + 3] == playerNumber:
       return columnNumber + 1
 
   def twoBlankOneInRow(playerNumber, columnNumber, rowState):
-    if rowState[columnNumber] == playerNumber and rowState[columnNumber + 1] == playerNumber and rowState[
+    if columnNumber <= 5 and rowState[columnNumber] == playerNumber and rowState[columnNumber + 1] == playerNumber and rowState[
     columnNumber + 2] == 0 and rowState[columnNumber + 3] == playerNumber:
       return columnNumber + 2
 
   def blankThreeInRow(playerNumber, columnNumber, rowState):
-    if rowState[columnNumber] == 0 and rowState[columnNumber + 1] == playerNumber and rowState[
+    if columnNumber <= 3 and rowState[columnNumber] == 0 and rowState[columnNumber + 1] == playerNumber and rowState[
     columnNumber + 2] == playerNumber and rowState[columnNumber + 3] == playerNumber:
       return columnNumber
